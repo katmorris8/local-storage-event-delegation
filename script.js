bfc48846b782313f8fccb15268d7c6ad889f38cd
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-06-14 15:04:22 -0400

    saved items to local storage

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = [];
+const items = JSON.parse(localStorage.getItem('items')) || [];
 
 function addItem(e) {
   e.preventDefault();
@@ -11,6 +11,7 @@ function addItem(e) {
   }
   items.push(item);
   populateList(items, itemsList);
+  localStorage.setItem('items', JSON.stringify(items));
   this.reset();
 }
 
@@ -26,3 +27,5 @@ function populateList(plates = [], platesList) {
 }
 
 addItems.addEventListener('submit', addItem)
+
+populateList(items, itemsList)
