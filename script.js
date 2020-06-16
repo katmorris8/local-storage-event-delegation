782e4fd5348f5206302c845388c8e80339fc6b83
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-06-15 21:22:03 -0400

    toggled the tacofied checks and updated local storage accordingly

--- script.js
+++ script.js
@@ -19,13 +19,23 @@ function populateList(plates = [], platesList) {
   platesList.innerHTML = plates.map((plate, i) => {
     return `
       <li>
-        <input type="checkbox" data-item=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
+        <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
         <label for="item${i}">${plate.text}</label>
       </li>
     `;
   }).join('');
 }
 
-addItems.addEventListener('submit', addItem)
+function toggleDone(e) {
+  if (!e.target.matches('input')) return;
+  const el = e.target;
+  const index = el.dataset.index;
+  items[index].done = !items[index].done;
+  localStorage.setItem('items', JSON.stringify(items));
+  populateList(items, itemsList);
+}
 
-populateList(items, itemsList)
+addItems.addEventListener('submit', addItem);
+itemsList.addEventListener('click', toggleDone);
+
+populateList(items, itemsList);
